a328e697a9a5540401928e3655e28474dc5da104
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-06 21:46:00 -0400

    basic css setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 import NoteEditor from "./components/NoteEditor";
 
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <Header />
-      <NotesList />
-      {/* <NoteEditor /> */}
+    <div className="appWrapper">
+      <div className="app">
+        <Header />
+        <NotesList />
+        {/* <NoteEditor /> */}
+      </div>
     </div>
   );
 }
