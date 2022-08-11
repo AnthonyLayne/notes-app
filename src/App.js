16a5576bf848b638b4576f35b083afaa236f8e52
AnthonyLayne <kepheus.ds@gmail.com>
2022-08-11 10:26:18 -0400

    added new route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Login from "./components/Login";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 import NoteEditor from "./components/NoteEditor";
@@ -10,6 +11,9 @@ import "./App.css";
 // www.notes.com/edit
 // www.notes.com/edit/asdlfkjadsasdfasf
 
+// will have to render login page or notes list based on if user is logged in or not
+// example: if !loggedIn ? Login : NotesList
+
 function App() {
   return (
     <Router>
@@ -18,7 +22,8 @@ function App() {
           <Header />
 
           <Routes>
-            <Route exact path="/" element={<NotesList />} />
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/notes" element={<NotesList />} />
             <Route exact path="edit/" element={<NoteEditor />} />
             <Route exact path="edit/:noteId" element={<NoteEditor />} />
           </Routes>
