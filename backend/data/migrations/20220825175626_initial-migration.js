aa8c175957bf50cdb751cc43dcb36290da762a22
AnthonyLayne <kepheus.ds@gmail.com>
2022-08-29 12:48:45 -0400

    test db structure

--- backend/data/migrations/20220825175626_initial-migration.js
+++ backend/data/migrations/20220825175626_initial-migration.js
@@ -3,12 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-  await knex.schema.createTable("notes", (table) => {
-    table.increments("note_id");
-    table.string("note_title", 200).notNullable();
-    table.string("note_description", 200).notNullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-  });
+  await knex.schema
+    .createTable("notes", (table) => {
+      table.increments("note_id");
+      table.string("note_title", 200).notNullable();
+      table.string("note_description", 200).notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("users", (table) => {
+      table.increments("user_id");
+      table.string("user_username", 60).notNullable();
+      table.string("user_password", 60).notNullable();
+    });
 };
 
 /**
@@ -16,5 +22,6 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("users");
   await knex.schema.dropTableIfExists("notes");
 };
--- backend/data/seeds/01-make-notes.js
+++ backend/data/seeds/01-make-notes.js
@@ -1,9 +1,19 @@
 const notes = [
-  { note_id: 1 },
-  { note_title: "this is a test" },
-  { note_description: "it either worked or it didn't lol" },
+  {
+    note_id: 1,
+    note_title: "this is a test",
+    note_description: "it either worked or it didnt lol",
+  },
+];
+const users = [
+  {
+    user_id: 1,
+    user_username: "test",
+    user_password: "testpass",
+  },
 ];
 
 exports.seed = async function (knex) {
   await knex("notes").insert(notes);
+  await knex("users").insert(users);
 };
